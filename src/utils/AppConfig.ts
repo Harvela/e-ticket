f71a0094c48f3c566b9842201f58afd4fb6dd0a8
ciraganenicole <ciraganenicole@gmail.com>
2024-01-31 22:46:05 +0300

    Update

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
-  site_name: 'Docta Mobile',
-  title: 'Docta Mobile',
+  site_name: 'Harvely',
+  title: 'Harvely',
   description:
     'Santé accessible pour tous avec Docta Mobile. Téléconsultations, consultations à domicile et suivi, médecine familiale abordable. Afrique, Tech, Experts.',
   locale: 'fr',

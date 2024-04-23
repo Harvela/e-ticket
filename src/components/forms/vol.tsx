19087121b028bc8ca6658daf39ebfeae6916cab8
lemoissonM <murhulametre@gmail.com>
2024-04-23 09:20:32 +0200

    add multicity

--- src/components/forms/vol.tsx
+++ src/components/forms/vol.tsx
@@ -5,6 +5,7 @@ import { Button, Modal } from 'flowbite-react';
 import { useRouter } from 'next/router';
 import { useTranslation } from 'next-i18next';
 import React, { useState } from 'react';
+import { PlusSquare, Trash } from 'react-feather';
 import type { SubmitHandler } from 'react-hook-form';
 import { useForm } from 'react-hook-form';
 import { FaArrowLeft, FaArrowRight, FaWindowClose } from 'react-icons/fa';
@@ -26,6 +27,11 @@ export const FlyForm: React.FC<VolProps> = () => {
   const [openDrop, setOpenDrop] = useState<boolean>(false);
   const { register, handleSubmit, setValue, watch } = useForm<any>();
   const [passengerInfo, setPassengerInfo] = useState<any>({});
+
+  const [steps, setSteps] = useState<number>(1);
+  const [currentStep, setCurrentStep] = useState<number>(0);
+  const [flightInfo, setFlightInfo] = useState<any>([]);
+
   const onSubmit: SubmitHandler<any> = (data) => {
     if (
       !data.origin ||
@@ -36,6 +42,7 @@ export const FlyForm: React.FC<VolProps> = () => {
       setErrors({ message: 'Veuillez completer tous les champs' });
       return;
     }
+
     const flightData = [
       {
         place_depart: data.origin,
@@ -43,12 +50,22 @@ export const FlyForm: React.FC<VolProps> = () => {
         date: data.departureDate,
       },
     ];
-    if (data.arrivalDate && selectedTab === 2)
+
+    if (data.arrivalDate && selectedTab === 2) {
       flightData.push({
         place_depart: data.destination,
         place_arrival: data.origin,
         date: data.arrivalDate,
       });
+    }
+
+    if (currentStep < steps - 1) {
+      setFlightInfo([...flightInfo, ...flightData]);
+      setCurrentStep(currentStep + 1);
+    } else {
+      setFlightInfo([...flightInfo, ...flightData]);
+    }
+
     localStorage.setItem(
       'reservation',
       JSON.stringify({
@@ -70,19 +87,8 @@ export const FlyForm: React.FC<VolProps> = () => {
     );
   };
 
-  // const mutation = useMutation(fetchFlights, {
-  //   onSuccess: (data) => {
-  //     // redirect if flights found
-  //   },
-  //   onError: (error) => {
-  //     console.log(error);
-  //     // show error message
-  //   },
-  // });
+  const stepArray = Array.from({ length: steps }, (_, i) => i);
 
-  // const onSubmit = (data: any) => {
-  //   mutation.mutate(data);
-  // };
   return (
     <div className="w-full">
       <Modal show={!!errors.message} onClose={() => setErrors({})}>
@@ -130,109 +136,134 @@ export const FlyForm: React.FC<VolProps> = () => {
           </button>
         </div>
       </div>
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="grid grid-cols-1 items-center justify-between gap-4 rounded-b-[15px] bg-[#EAF0F0] p-4 md:flex md:flex-row md:justify-evenly md:pt-4 lg:flex lg:flex-row lg:rounded-r-[15px] lg:px-4 lg:pt-4"
-      >
-        <PlaceInput
-          name="origin"
-          label={t('flights.from')}
-          icon={<SlArrowDown />}
-          placeholder="Kinshasa"
-          onChange={(e: any) => {
-            setValue('origin', e);
-          }}
-          excludedOptions={[watch('destination')]}
-        />
-        <PlaceInput
-          name="destination"
-          label={t('flights.to')}
-          icon={<SlArrowDown />}
-          placeholder="Kolwezi"
-          onChange={(e: any) => {
-            setValue('destination', e);
-          }}
-          excludedOptions={[watch('origin')]}
-        />
-        <Input
-          name="departureDate"
-          type="date"
-          label={t('flights.departure')}
-          placeholder="Goma"
-          style=" lg:pl-4"
-          register={register}
-          min={dayjs().format('YYYY-MM-DD')}
-          onChange={(e: any) => {
-            setValue('departureDate', e);
-          }}
-        />
-        {selectedTab === 2 && (
-          <Input
-            name="arrivalDate"
-            type="date"
-            label={t('flights.return')}
-            onChange={(e: any) => {
-              setValue('arrivalDate', e);
-            }}
-            placeholder="Goma"
-            style="lg:pl-4"
-            register={register}
-            min={dayjs(watch('departureDate')).format('YYYY-MM-DD')}
-          />
-        )}
-
-        <div className="relative">
-          <span className="mb-2 flex flex-row items-center gap-1 text-[12px] font-semibold text-blue">
-            {t('passenger.passengers')}
-          </span>
-          <button
-            onClick={() => setOpenDrop(!openDrop)}
-            type="button"
-            className="flex w-full flex-row items-center rounded-[5px] border-blue/10 bg-blue/5 px-4 py-2 text-[16px] focus:outline-0 md:text-[14px]"
-          >
-            {(passengerInfo.adult || 0) + (passengerInfo.children || 0)} -{' '}
-            {[passengerInfo.classe]}
-            <svg
-              className="ms-3 h-2.5 w-2.5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 10 6"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="m1 1 4 4 4-4"
-              />
-            </svg>
-          </button>
-          {openDrop && (
-            <div className="absolute bottom-full left-0 flex flex-row rounded-[5px] border border-gray-200 bg-white p-8 shadow-lg">
-              <Passenger
-                onPassengerChange={(adult, children, classe) => {
-                  setPassengerInfo({ adult, children, classe });
-                }}
-              />
-              <FaWindowClose
-                onClick={() => setOpenDrop(false)}
-                className="h-6 w-6 text-blue"
-              />
-            </div>
-          )}
-        </div>
-
-        <button
-          className="mt-6 h-[35px] rounded-[5px] bg-blue px-4 py-[5px] text-sm text-white"
-          // onClick={() => {
-          //   navigation.push('/reservation');
-          // }}
-          type="submit"
+      {stepArray.map((flight: any, index: number) => (
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          key={`flight-${index}`}
+          className="grid grid-cols-1 items-center justify-between gap-4 rounded-b-[15px] bg-[#EAF0F0] p-4 md:flex md:flex-row md:justify-evenly md:pt-4 lg:flex lg:flex-row lg:rounded-r-[15px] lg:px-4 lg:pt-4"
         >
-          {t('flights.search')}
-        </button>
-      </form>
+          <PlaceInput
+            name="origin"
+            label={t('flights.from')}
+            icon={<SlArrowDown />}
+            placeholder="Kinshasa"
+            onChange={(e: any) => {
+              setValue('origin', e);
+            }}
+            excludedOptions={[watch('destination')]}
+            defaultValue={flightInfo[flight]?.place_arrival}
+            disabled={index !== currentStep}
+          />
+          <PlaceInput
+            name="destination"
+            label={t('flights.to')}
+            icon={<SlArrowDown />}
+            placeholder="Kolwezi"
+            onChange={(e: any) => {
+              setValue('destination', e);
+            }}
+            excludedOptions={[watch('origin')]}
+            defaultValue={flightInfo[flight]?.place_depart}
+            disabled={index !== currentStep}
+          />
+          <Input
+            name="departureDate"
+            type="date"
+            label={t('flights.departure')}
+            placeholder="Goma"
+            style=" lg:pl-4"
+            register={register}
+            min={dayjs().format('YYYY-MM-DD')}
+            onChange={(e: any) => {
+              setValue('departureDate', e);
+            }}
+            defaultValue={flightInfo[flight]?.date}
+            disabled={index !== currentStep}
+          />
+          {selectedTab === 2 && (
+            <Input
+              name="arrivalDate"
+              type="date"
+              label={t('flights.return')}
+              onChange={(e: any) => {
+                setValue('arrivalDate', e);
+              }}
+              placeholder="Goma"
+              style="lg:pl-4"
+              register={register}
+              min={dayjs(watch('departureDate')).format('YYYY-MM-DD')}
+            />
+          )}
+
+          <div className="relative">
+            <span className="mb-2 flex flex-row items-center gap-1 text-[12px] font-semibold text-blue">
+              {t('passenger.passengers')}
+            </span>
+            <button
+              onClick={() => setOpenDrop(!openDrop)}
+              type="button"
+              className="flex w-full flex-row items-center rounded-[5px] border-blue/10 bg-blue/5 px-4 py-2 text-[16px] focus:outline-0 md:text-[14px]"
+            >
+              {(passengerInfo.adult || 0) + (passengerInfo.children || 0)} -{' '}
+              {[passengerInfo.classe]}
+              <svg
+                className="ms-3 h-2.5 w-2.5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 10 6"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="m1 1 4 4 4-4"
+                />
+              </svg>
+            </button>
+            {openDrop && (
+              <div className="absolute bottom-full left-0 flex flex-row rounded-[5px] border border-gray-200 bg-white p-8 shadow-lg">
+                <Passenger
+                  onPassengerChange={(adult, children, classe) => {
+                    setPassengerInfo({ adult, children, classe });
+                  }}
+                />
+                <FaWindowClose
+                  onClick={() => setOpenDrop(false)}
+                  className="h-6 w-6 text-blue"
+                />
+              </div>
+            )}
+          </div>
+          <PlusSquare
+            className="mt-5 h-[24px] w-[24px] text-blue"
+            onClick={() => {
+              setSteps(steps + 1);
+              setCurrentStep(currentStep + 1);
+            }}
+          />
+          {index !== 0 && (
+            <Trash
+              className="mt-5 h-[24px] w-[24px] text-blue"
+              onClick={() => {
+                setSteps(steps + 1);
+                setCurrentStep(currentStep + 1);
+              }}
+            />
+          )}
+
+          <button
+            className="mt-6 h-[35px] rounded-[5px] bg-blue px-4 py-[5px] text-sm text-white"
+            // onClick={() => {
+            //   navigation.push('/reservation');
+            // }}
+            type="submit"
+          >
+            {t('flights.search')}
+          </button>
+        </form>
+      ))}
     </div>
   );
 };
